cdc33e36f0822173f64cb278d28826cf7babb552
Michael Albertson <mdalbertsonjr@gmail.com>
2015-04-29 18:56:53 -0700

    Updated the chocopacks script.

--- vagrant-image/scripts/chocopacks.bat
+++ vagrant-image/scripts/chocopacks.bat
@@ -1,6 +1,6 @@
 :: Ensure C:\Chocolatey\bin is on the path
 set /p PATH=<C:\Windows\Temp\PATH
-
+cmd /c choco feature enable -n allowGlobalConfirmation
 :: Install all the things; for example:
 :: cmd /c choco install 7zip
 :: cmd /c choco install notepadplusplus

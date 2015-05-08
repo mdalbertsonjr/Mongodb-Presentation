7fd023cc9a0c915696fb31125f57a3282379f54a
Michael Albertson <mdalbertsonjr@gmail.com>
2015-05-07 19:59:59 -0700

    Started adding queries to the controller.

--- MongoDBDemo/MapApplication/Controllers/HomeController.cs
+++ MongoDBDemo/MapApplication/Controllers/HomeController.cs
@@ -3,12 +3,13 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MapApplication.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : AsyncController
     {
         //
         // GET: /Home/
@@ -31,19 +32,44 @@ namespace MapApplication.Controllers
         
 
         [HttpGet]
-        public JsonResult Layer(string id)
+        public async Task<JsonResult> Layer(string id)
         {
-            //Get the collection named by the id
             IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(id);
-            //Get all of the features in the collection
-            var features = collection.Find<BsonDocument>((x) => true);
-            //Reformat as necessary for returning the collection as json. GeoJSON?
-            return Json(new { }, JsonRequestBehavior.AllowGet);
+
+            var features = await collection.Find<BsonDocument>(new BsonDocument()).ToListAsync();
+
+            return Json(features.ToJson(),JsonRequestBehavior.AllowGet);
         }
 
+        //http://localhost:57212/home/near/default?point=-130.83901183425886&point=40.355523246501896&maxDistance=500&minDistance=250
         [HttpGet]
-        public JsonResult Near(double[] point, string collectionName)//Make a model for this argument
+        public async Task<JsonResult> Near(string id, [System.Web.Http.FromUri] double[] point, double maxDistance, double minDistance)//Make a model for this argument
         {
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(id);
+
+            var nearQuery = new BsonDocument();
+            nearQuery["$near"] = new BsonDocument();
+            nearQuery["$near"]["$geometry"] = (new
+            {
+                type = "Point",
+                coordinates = point,
+            }).ToBsonDocument();
+            nearQuery["$near"]["$maxDistance"] = maxDistance;
+            nearQuery["$near"]["$minDistance"] = minDistance;
+
+            var query = new BsonDocument("geo", nearQuery);
+
+            var features = await collection.Find(query).ToListAsync();
+            features = features.Take(3).ToList();
+
+            return Json(features.ToJson(), JsonRequestBehavior.AllowGet);
+        }
+
+        //http://docs.mongodb.org/manual/reference/operator/query/geoWithin/#op._S_geoWithin
+        [HttpGet]
+        public async Task<JsonResult> Within(double[] point, string collectionName)//Make a model for this argument
+        {
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(collectionName);
             //set up the query
             //query the database
             //reformat the response
@@ -51,17 +77,9 @@ namespace MapApplication.Controllers
         }
 
         [HttpGet]
-        public JsonResult Within(double[] point, string collectionName)//Make a model for this argument
+        public async Task<JsonResult> Intersects(double[] point, string collectionName)//Make a model for this argument
         {
-            //set up the query
-            //query the database
-            //reformat the response
-            return Json(new { }, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public JsonResult Intersects(double[] point, string collectionName)//Make a model for this argument
-        {
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(collectionName);
             //set up the query
             //query the database
             //reformat the response

e6b546c7f903548e13a0944db6b236a2903cb46f
Michael Albertson <mdalbertsonjr@gmail.com>
2015-05-09 21:06:39 -0700

    Last commit for tonight.

--- MongoDBDemo/MapApplication/Controllers/HomeController.cs
+++ MongoDBDemo/MapApplication/Controllers/HomeController.cs
@@ -42,9 +42,8 @@ namespace MapApplication.Controllers
             return Json(features.ToJson(),JsonRequestBehavior.AllowGet);
         }
 
-        //http://localhost:57212/home/near/default?point=-130.83901183425886&point=40.355523246501896&maxDistance=500&minDistance=250
-        [HttpGet]//[HttpPost]              string id, [FromBody]dynamic obj
-        public async Task<JsonResult> Near(string id, [System.Web.Http.FromUri] double[] point, double maxDistance, double minDistance)//Make a model for this argument
+        [HttpGet]
+        public async Task<JsonResult> Near(string id, [System.Web.Http.FromUri]double[] point,  double maxDistance, double minDistance)
         {
             IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(id);
 
@@ -58,40 +57,33 @@ namespace MapApplication.Controllers
             nearQuery["$near"]["$maxDistance"] = maxDistance;
             nearQuery["$near"]["$minDistance"] = minDistance;
 
-            var query = new BsonDocument("geo", nearQuery);
+            var query = new BsonDocument("geometry", nearQuery);
 
             var features = await collection.Find(query).ToListAsync();
-            features = features.Take(3).ToList();
+            features = features.Select(x => { x.Remove("_id"); return x; }).ToList();
 
             return Json(features.ToJson(), JsonRequestBehavior.AllowGet);
         }
 
-        //http://docs.mongodb.org/manual/reference/operator/query/geoWithin/#op._S_geoWithin
-        //This dynamic argument might not work...
-        //http://stackoverflow.com/questions/19616406/pass-dynamic-data-to-mvc-controller-with-ajax
-        //http://stackoverflow.com/questions/5022958/passing-dynamic-json-object-to-c-sharp-mvc-controller
-        //I really like the dynamic json wrapper in the second link...
         [HttpGet]
-        public async Task<JsonResult> Within(string id, [System.Web.Http.FromBody]dynamic obj)//double[] point, string collectionName)//Make a model for this argument
+        public async Task<JsonResult> WithinBox(string id, [System.Web.Http.FromUri] double[] leftCorner, [System.Web.Http.FromUri] double[] rightCorner)
         {
             IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(id);
-            //set up the query
-            //query the database
-            //reformat the response
-            return Json(new { }, JsonRequestBehavior.AllowGet);
-        }
 
-        [HttpGet]
-        public async Task<JsonResult> Intersects(string id, [System.Web.Http.FromBody]dynamic obj)//double[] point, string collectionName)//Make a model for this argument
-        {
-            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>(id);
-            //set up the query
-            //query the database
-            //reformat the response
-            return Json(new { }, JsonRequestBehavior.AllowGet);
-        }
+            var withinQuery = new BsonDocument();
+            withinQuery["$geoWithin"] = new BsonDocument();
+            var bsonLeftCorner = new BsonArray(leftCorner);
+            var bsonRightCorner = new BsonArray(rightCorner);
+            var bsonCorners = new BsonArray() { bsonLeftCorner, bsonRightCorner };
+            withinQuery["$geoWithin"]["$box"] = bsonCorners;
 
-        //Intersects with line?
-        //Intersects with polygon?
+            var query = new BsonDocument("geometry", withinQuery);
+
+            var temp = query.ToJson();
+            var features = await collection.Find(query.ToBsonDocument()).ToListAsync();
+            features = features.Select(x => { x.Remove("_id"); return x; }).ToList();
+
+            return Json(features.ToJson(), JsonRequestBehavior.AllowGet);
+        }
     }
 }

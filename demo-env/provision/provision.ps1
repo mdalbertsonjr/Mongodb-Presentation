5452a6b1dfc606a31f1943dfab1f9d7ca435795a
Michael Albertson <mdalbertsonjr@gmail.com>
2015-05-11 15:54:47 -0700

    Changed the url for downloading MongoDB.

--- demo-env/provision/provision.ps1
+++ demo-env/provision/provision.ps1
@@ -1,2 +1,2 @@
 echo "Downloading MongoDB"
-(New-Object System.Net.WebClient).DownloadFile('https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.0.2-signed.msi', '/users/vagrant/mongo.msi')
+(New-Object System.Net.WebClient).DownloadFile('http://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.0.2-signed.msi', '/users/vagrant/mongo.msi')
